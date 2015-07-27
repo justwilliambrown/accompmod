bb7360c52458f958761b3f16140f9ca5babae261
John DOE <juswilliambrown101@gmail.com>
2015-07-27 13:22:00 +0800

    Changes to be committed: 	modified:   compmod.lua Fixed systems. Maps can only be chosen by members of that team. Ready up and ready down work perfectly. Match system is functional, inter system still needs work. All players need to ready up, then teams are shuffled. Then maps are selected, and the !inter command is again run. Shit works.

--- compmod.lua
+++ compmod.lua
@@ -3,133 +3,196 @@
 PLUGIN_NAME = "Competitive Assaultcube Plugin"
 PLUGIN_AUTHOR = "Padfoot"
 PLUGIN_VERSION = "0.1"
---what
+--ToDo: Fix ready system. Fix match system so second map is played.
 include("ac_server")
 
 function slice(array, S, E)
-  local result = {}
-  local length = #array
-  S = S or 1
-  E = E or length
-  if E < 0 then
-    E = length + E + 1
-  elseif E > length then
-    E = length
-  end
-  if S < 1 or S > length then
-    return {}
-  end
-  local i = 1
-  for j = S, E do
-    result[i] = array[j]
-    i = i + 1
-  end
-  return result
+	local result = {}
+	local length = #array
+	S = S or 1
+	E = E or length
+	if E < 0 then
+		E = length + E + 1
+	elseif E > length then
+		E = length
+	end
+	if S < 1 or S > length then
+		return {}
+	end
+	local i = 1
+	for j = S, E do
+		result[i] = array[j]
+		i = i + 1
+	end
+	return result
 end
 
-function inter(time, map)
-	a = math.random(3, 10)
-	count = 0
-	while count <= a do
-		shuffleteams()
-		count = count + 1
-		end
-	say("Teams have been shuffled"..a.."times.")
-	setautoteam(0)
-	say("AutoTeam has been turned off")
-	match = true
-	changemap(map, 5, time)
-	say("Good Luck and Have Fun!!!")
+function inter(time)
+	setautoteam(false)
+  if getautoteam() == false then
+	   say("AutoTeam has been turned off")
+	else
+    say("Shit, something broke.")
+  end
+  match = true
+  if hasShuffled == false then
+		a = math.random(3, 10)
+  	count = 0
+  	while count <= a do
+    	shuffleteams()
+    	count = count + 1
+  	end
+		hasShuffled = true
+		say("Teams have been shuffled "..a.." times.")
+	end
+	if redmap ~= '' and blumap ~= '' then
+		changemap(redmap, 5, time)
+		setmastermode(2)
+		say("Good Luck and Have Fun!!!")
+		say("Mastermode is "..getmastermode())
+		hasShuffled = false
+	else
+		say("Not all maps have been selected")
+end
 end
 
-function match(time, map)
-	getPlayers()
-	setautoteam(0)
-	say("AutoTeam has been turned off")
+function playmatch(time)
+	setautoteam(false)
+	if getautoteam() == false then
+		 say("AutoTeam has been turned off")
+	else
+		say("Shit, something broke.")
+	end
 	match = true
-	changemap(map, 5, time)
+	if stage == 1 then
+		changemap(blumap, 5, time)
+		stage = 2
+	else
+		changemap(redmap, 5, time)
+		stage = 1
+	end
 	say("Good Luck and Have Fun!!!")
+	setmastermode(2)
+	say("Mastermode is "..getmastermode())
 end
 
 function getPlayers()
 	for player_cn in rvsf() do
 		--add the player to the nplayers array
-		table.insert(nplayers, tostring(player_cn))
+		table.insert(nplayers, player_cn)
 	end
 	for player_cn in cla() do
 		--add the player to nplayers
-		table.insert(nplayers, tostring(player_cn))
+		table.insert(nplayers, player_cn)
 	end
 	-- Get number of players
 	ppl = table.getn(nplayers)
-	numplay = ppl
-	say(numplay)
+	say(ppl.." players here.")
 end
 
 function setready(cn)
-	table.remove(nplayers, tostring(cn))
-	say("There are ".." new")
+	local i = get_key(nplayers, cn)
+	table.remove(nplayers, i)
 	string = getname(cn).." is now \fLready."
 	say(string)
+	num = #nplayers
+	say("There are "..num.." players who are not ready")
 end
 
 function notready(cn)
-	table.insert(nplayers, tostring(cn))
-	say(table.getn(nplayers))
+	print(get_key(nplayers, cn))
+	if get_key(nplayers, cn) then return end
+  table.insert(nplayers, cn)
 	string = getname(cn).." is \f3not \f3ready."
 	say(string)
+	num = #nplayers
+	say("There are "..num.." players who are not ready")
 end
 
 function say(text, cn)
-  if cn == nil then cn = -1 end -- to all
-  clientprint(cn, text)
+	if cn == nil then cn = -1 end -- to all
+	clientprint(cn, text)
 end
 
+function setmap(cn, team, map)
+	if team == "CLA" then
+		if getteam(cn) == 0 then
+			redmap = map
+			say("Team "..team.." has selected "..map)
+		end
+	elseif team == "RVSF" then
+		if getteam(cn) == 1 then
+			blumap = map
+			say("Team "..team.." has selected "..map)
+		end
+	else
+		say("Something broke!")
+	end
+end
+
+function get_key(tab, value)
+  for key, val in pairs(tab) do
+    if val == value then
+      return key
+    end
+  end
+	return nil
+end
 --function playerSay(cn, text)
 --	text2 = string.format("Log: Player %s says: %s. Their IP is: %s", getname(cn), text, getip(cn))
 --	logline(4, text2)
 --	if co
 
---Variables go here
+--Varilables go here
 nplayers = {}
-numplay = 0
 match = false
+stage = 0
+blumap = ''
+redmap = ''
+hasShuffled = false
+
 --Commands go here :)
 commands =
 {
-  [".ready"] = {
+	["!setmap"] = {
+	function (cn, args)
+		--Usage !setmap <team> <map>
+		setmap(cn, args[1], args[2])
+	end
+	};
+
+	["!ready"] = {
 	function (cn)
-		if nplayers[tostring(cn)] ~= nil then
+		if get_key(nplayers, cn) ~= nil then
 			setready(cn)
 		end
 	end
 	};
 
-	[".notready"] = {
+	["!notready"] = {
 		function (cn)
-			if nplayers[tostring(cn)] == nil then
-				notready(cn)
-			end
+			if get_key(nplayers, cn) then return end
+			notready(cn)
 		end
 	};
 
 
-	[".inter"] = {
+	["!inter"] = {
 	 function (cn, args)
-	 	say(table.getn(nplayers))
 		if table.getn(nplayers) == 0 then
-			inter(args[2], args[1])
-		else
 			say("Not all players are ready!")
 		end
-	 end
-
+	end
 	};
 
-	[".match"] = {
+	["!match"] = {
 	 function (cn, args)
 		if table.getn(nplayers) == 0 then
-			match(args[2], args[1])
+			if blumap ~= '' and redmap ~= '' then
+				playmatch(args[1])
+			else
+				say("Not all maps have been selected!")
+			end
 		else
 			say("Not all players are ready!")
 		end
@@ -151,55 +214,60 @@ function onPlayerSayText(cn, text)
 
 	if commands[command] ~= nil then
 		local callback = commands[command][1]
-        -- If there is a command there, carry out the command
-        callback(cn, args)
-    end
-	  if isadmin(cn) then
-	  	SayToAllA(text,cn)
-	  	return PLUGIN_BLOCK
-	  else
-      	SayToAll(text,cn)
-      	return PLUGIN_BLOCK
-	  end
+				-- If there is a command there, carry out the command
+				callback(cn, args)
+		end
+		if isadmin(cn) then
+			SayToAllA(text,cn)
+			return PLUGIN_BLOCK
+		else
+				SayToAll(text,cn)
+				return PLUGIN_BLOCK
+		end
 end
 
 function SayToAll(text, except)
-   for n=0,20,1 do
-    -- if isconnected(n) and n ~= except then
+	 for n=0,20,1 do
+		-- if isconnected(n) and n ~= except then
 	if dup then
-      say("\f4|\fX" .. except .. "\f4|\fR#\f5" .. getname(except) .. ":\f9 " .. text,n)
-	  elseif isconnected(n) and n ~= except then
-	   say("\f4|\fX" .. except .. "\f4|\fR#\f5" .. getname(except) .. ":\f9 " .. text,n)
+			say("\f4|\fX" .. except .. "\f4|\fR#\f5" .. getname(except) .. ":\f9 " .. text,n)
+		elseif isconnected(n) and n ~= except then
+		 say("\f4|\fX" .. except .. "\f4|\fR#\f5" .. getname(except) .. ":\f9 " .. text,n)
 	 end
-	--  end
+	--	end
 
-     end
-   end
+		 end
+	 end
 
 function SayToAllA(text, except)
-   for n=0,20,1 do
-    -- if isconnected(n) and n ~= except then
+	 for n=0,20,1 do
+		-- if isconnected(n) and n ~= except then
 	if dup then
-      say("\f4|\fX" .. except .. "\f4|\f3#\f5" .. getname(except) .. ":\f9 " .. text,n)
-	  elseif isconnected(n) and n ~= except then
-	  say("\f4|\fX" .. except .. "\f4|\f3#\f5" .. getname(except) .. ":\f9 " .. text,n)
+			say("\f4|\fX" .. except .. "\f4|\f3#\f5" .. getname(except) .. ":\f9 " .. text,n)
+		elseif isconnected(n) and n ~= except then
+		say("\f4|\fX" .. except .. "\f4|\f3#\f5" .. getname(except) .. ":\f9 " .. text,n)
 	 end
-	--  end
+	--	end
 
-     end
-   end
+		 end
+	 end
 
 function SayToAll2(text, except)
-   if isconnected(n) and n ~= except then
-      say("\fR[SERVER INFO] \f4",text,n)
+	 if isconnected(n) and n ~= except then
+			say("\fR[SERVER INFO] \f4",text,n)
 	end
 end
 
 function onMapEnd()
 	--Reset all of the values
 	nplayers = {}
-	numplay = 0
 	match = false
+  setmastermode(0)
+	blumap = ''
+	redmap = ''
+	if stage == 1 then
+		playmatch()
+	end
 end
 
 function onMapChange(map, mode)
@@ -211,16 +279,13 @@ function onPlayerTeamChange(cn, team, reason)
 	if match == true then
 		if team == 1 or team == 0 then
 			say(getname(cn)..", they are playing a match. You can't join.", cn)
-			setteam(cn, 4, 1)
-			setteam(cn, 4, 1)
-			setteam(cn, 4, 1)
-			setteam(cn, 4, 1)
-		end
-	end
+      return PLUGIN_BLOCK, false
+    end
+  end
 end
 
 function onPlayerConnect(cn)
-   say("\f4Hello \fR" .. getname(cn) .. "!")
+	 say("\f4Hello \fR" .. getname(cn) .. "!")
 -- say("\fR [SERVER INFO]" .. getname(cn) .. "\fR connected!!! with ip \f4" .. getip(cn) .. "")
 setautoteam(false)
 end
